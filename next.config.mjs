690ff3954b35f5a03520076d211c3946c0155b8a
Baroen Sudarman <baroenese@gmail.com>
2024-06-16 19:42:19 +0800

    Oke 🔥

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
-    distDir: "website",
-    basePath: "/website-flashdisk-hiburan-mobil"
+    // output: "export",
+    // images: {
+    //     unoptimized: true,
+    // },
+    // distDir: "website",
+    // basePath: "/website-flashdisk-hiburan-mobil",
+    // assetPrefix: "https://baroenese.github.io/website-flashdisk-hiburan-mobil",
+    // trailingSlash: true,
+    // skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
